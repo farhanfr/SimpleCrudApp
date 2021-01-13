54cf26922e6c115f5095dd2e37b96b23a1e296bc
farhanfitrahtur <farhanfitrahtur@gmail.com>
2021-01-13 10:57:30 +0700

    finish crud

--- src/pages/LocalApi/index.js
+++ src/pages/LocalApi/index.js
@@ -1,17 +1,21 @@
 import axios from 'axios'
-import React, { useState } from 'react'
-import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Alert, Button, Image, ScrollView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View, VirtualizedList } from 'react-native'
 
-const Item = () => {
+const Item = ({name,email,bidang,onPress,onDelete}) => {
     return (
         <View style={styles.itemContainer}>
-            <Image source={{ uri: 'https://ui-avatars.com/api/?size=150' }} style={styles.avatar} />
+            <TouchableOpacity onPress={onPress}>
+                <Image source={{ uri: `https://ui-avatars.com/api/?name=${name}&size=150`}} style={styles.avatar} />
+            </TouchableOpacity>
             <View style={styles.desc}>
-                <Text style={styles.name}>Nama Lengkap</Text>
-                <Text style={styles.email}>Email</Text>
-                <Text style={styles.bidang} >Bidang</Text>
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.email}>{email}</Text>
+                <Text style={styles.bidang} >{bidang}</Text>
             </View>
-            <Text style={styles.delete}>X</Text>
+            <TouchableOpacity>
+                <Text style={styles.delete} onPress={onDelete}>X</Text>
+            </TouchableOpacity>
         </View>
     )
 
@@ -21,29 +25,94 @@ const LocalApi = () => {
     const[name,setName]=useState("")
     const[email,setEmail]=useState("")
     const[bidang,setBidang]=useState("")
+    const[users,setUsers]=useState([])
+    const[button,setButton]=useState("Simpan")
+    const[selectedUser,setSelectedUser]=useState()
+    
+    useEffect(() => {
+        let isSubscriber = true
+        isSubscriber ? getData() : null
+        return () => isSubscriber = false
+    }, [])
 
     const submit = () =>{
         const data = {
             name,email,bidang
         }
         console.log(data)
-        axios.post("http://192.168.1.4:3000/users",data)
-        .then(res=>{console.log(res)})
-        setName("")
-        setEmail("")
-        setBidang("")
+
+        if (button === 'Simpan') {
+            axios.post("http://192.168.1.4:3000/users",data)
+            .then(res=>{console.log(res)})
+            setName("")
+            setEmail("")
+            setBidang("")
+            getData()    
+        }else if (button === 'Update') {
+            axios.put(`http://192.168.1.4:3000/users/${selectedUser.id}`,data)
+            .then(res=>{
+                console.log("update behasil")
+                setName("")
+                setEmail("")
+                setBidang("")
+                getData()    
+                setButton("Simpan")
+            })
+        }
+
+        
+    }
+
+    const getData = () =>{
+        axios.get("http://192.168.1.4:3000/users")
+        .then(res=>{
+            console.log('res :' + res.data)
+            setUsers(res.data)
+        })
+    }
+
+    const deleteData = (item) =>{
+        axios.delete(`http://192.168.1.4:3000/users/${item.id}`)
+        .then(res=>{
+            console.log("hapus berhasil")
+            getData()
+        })
+    }
+    
+    const selectItem = (item) =>{
+        console.log('selected item : ' + item)
+        setSelectedUser(item)
+        setName(item.name)
+        setEmail(item.email)
+        setBidang(item.bidang)
+        setButton("Update")
     }
 
     return (
         <View style={styles.container}>
+            
             <Text style={styles.textTitle}>Local api (json server)</Text>
             <Text>Masukkan User</Text>
             <TextInput placeholder="Nama" style={styles.input} value={name} onChangeText={(value)=>setName(value)} />
             <TextInput placeholder="Email" style={styles.input} value={email} onChangeText={(value)=>setEmail(value)} />
             <TextInput placeholder="Bidang" style={styles.input} value={bidang} onChangeText={(value)=>setBidang(value)} />
-            <Button title="Tambah" onPress={submit} />
+            <Button title={button} onPress={submit} />
             <View style={styles.line} />
-            <Item />
+            <View></View>
+            {users.map(user=>{
+               return (
+                <Item 
+                key={user.id} 
+                name={user.name} 
+                email={user.email} 
+                bidang={user.bidang} 
+                onPress={()=>selectItem(user)}
+                onDelete={()=> 
+                    Alert.alert('notice','Are you sure delete?',
+                    [{text:'tidak',onPress:()=>console.log('tidak hapus')},
+                    {text:'hapus',onPress:()=>deleteData(user)}])}/>
+               )
+            })}
         </View>
     )
 }
